5dd712a5cdcb6b8aeb93831f932a31fac81bf811
weegetiago <weegetiago@gmail.com>
2020-03-30 20:58:45 -0300

    #2058300320

--- omniStack11-frontend/src/pages/Cases/index.js
+++ omniStack11-frontend/src/pages/Cases/index.js
@@ -1,8 +1,68 @@
 import React from 'react';
-//import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { FiPower, FiTrash2 } from 'react-icons/fi';
 
-export default function Cases(){
-    return(
-        <h1>Casos</h1>
+import './styles.css';
+import logo from '../../assents/logo.svg';
+
+export default function Cases() {
+    return (
+        <div className="cases-container">
+            <header>
+                <img src={logo} alt="Be the Hero" />
+                <span>Bem vinda, Nome da ONG</span>
+
+                <Link className="button" to="cases/new">Cadastrar novo casos</Link>
+                <button type="button">
+                    <FiPower size={18} color="#e02041" />
+                </button>
+            </header>
+
+            <h1>Casos cadastrados</h1>
+            <ul>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso cadela baia</p>
+
+                    <strong>DESCRIÇÂO</strong>
+                    <p>Atropelada na rua Blumenau</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso cadela baia</p>
+
+                    <strong>DESCRIÇÂO</strong>
+                    <p>Atropelada na rua Blumenau</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+                <li>
+                    <strong>CASO:</strong>
+                    <p>Caso cadela baia</p>
+
+                    <strong>DESCRIÇÂO</strong>
+                    <p>Atropelada na rua Blumenau</p>
+
+                    <strong>VALOR</strong>
+                    <p>R$120,00</p>
+
+                    <button type="button">
+                        <FiTrash2 size={20} color="#a8a8b3" />
+                    </button>
+                </li>
+            </ul>
+        </div>
     );
 }
