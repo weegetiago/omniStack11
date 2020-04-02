c16c2c47969df519ff544ce0fc137ce3cf46bac9
weegetiago <weegetiago@gmail.com>
2020-04-02 13:24:03 -0300

    1323020420

--- omniStack11-frontend/src/pages/Logon/index.js
+++ omniStack11-frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import {FiLogIn} from 'react-icons/fi';
-import {Link} from 'react-router-dom';
-import React from 'react';
+import React, { useState } from 'react';
+import { FiLogIn } from 'react-icons/fi';
+import { Link, useHistory } from 'react-router-dom';
+
+import api from '../../servicos/api';
 
 import './styles.css';
 
@@ -8,14 +10,41 @@ import heroes from '../../assents/heroes.png';
 import logo from '../../assents/logo.svg';
 
 export default function Logon() {
+    const [id, setID] = useState('');
+    const history = useHistory();
+
+    async function listLogon(event) {
+        event.preventDefault();
+
+        const data = {
+            id,
+        };
+
+        try {
+            const response = await api.post('sessao', { id });
+
+            localStorage.setItem('ongId', id);
+            localStorage.setItem('ongName', response.data.name);
+
+            history.push('/cases');
+        }
+        catch (err) {
+            alert('ID incorreto, tente outro ou corra para as colinas');
+        }
+    }
+
     return (
         <div className="logon-container">
             <section className="form">
                 <img src={logo} alt="Be The Hero" />
 
-                <form>
+                <form onSubmit={listLogon}>
                     <h1>Faça o Logon</h1>
-                    <input placeholder="Seu ID" />
+                    <input
+                        placeholder="ID visitantes: 2a057067"
+                        value={id}
+                        onChange={event => setID(event.target.value)}
+                    />
                     <button className="button" type="submit">Entrar</button>
 
                     <Link className="back-link" to="/register">
--- omniStack11-frontend/src/pages/NewCase/index.js
+++ omniStack11-frontend/src/pages/NewCase/index.js
@@ -1,12 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import api from '../../servicos/api';
 
 import './styles.css';
 
 import logo from '../../assents/logo.svg';
 
 export default function NewCase() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [value, setValue] = useState('');
+
+    const ongId = localStorage.getItem('ongId');
+
+    const history = useHistory(); //Refresh na tela
+
+    async function newCase(event) {
+        event.preventDefault();
+
+        const data = {
+            title,
+            description,
+            value,
+        };
+
+        try {
+            await api.post('incidents', data, {
+                headers: {
+                    Authorization: ongId,
+                    
+                }
+            })
+
+            history.push('/cases');
+        }
+        catch (err) {
+            alert('Erro no cadastro, tente novamente ou corra para as colinas');
+        }
+    }
+
     return (
         <div className="newcase-container">
             <div className="content">
@@ -22,10 +55,22 @@ export default function NewCase() {
                 </Link>
                 </section>
 
-                <form>
-                    <input placeholder="Titulo do case" />
-                    <textarea placeholder="Descrição" />
-                    <input placeholder="Valor em reais" />
+                <form onSubmit={newCase}>
+                    <input
+                        placeholder="Titulo do case"
+                        value={title}
+                        onChange={event => setTitle(event.target.value)}
+                    />
+                    <textarea
+                        placeholder="Descrição"
+                        value={description}
+                        onChange={event => setDescription(event.target.value)}
+                    />
+                    <input
+                        placeholder="Valor em reais"
+                        value={value}
+                        onChange={event => setValue(event.target.value)}
+                    />
                     <button className="button" type="submit">Cadastrar</button>
                 </form>
             </div>
